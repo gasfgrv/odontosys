2ac579b33341d294703b256bd30d4fba099e9231
Gustavo Silva <gustavo_almeida11@hotmail.com>
2022-12-11 15:35:39 -0300

    ms-endereco: injeção de dependencias no usecase de buscar cep

--- ms-endereco/src/main/java/br/com/gusta/odontosys/msendereco/domain/usecases/BuscarEndereco.java
+++ ms-endereco/src/main/java/br/com/gusta/odontosys/msendereco/domain/usecases/BuscarEndereco.java
@@ -4,11 +4,12 @@ import br.com.gusta.odontosys.msendereco.domain.entities.Endereco;
 import br.com.gusta.odontosys.msendereco.domain.repositories.EnderecoRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
-@RequiredArgsConstructor
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class BuscarEndereco {
 
     private final EnderecoRepository enderecoRepository;
