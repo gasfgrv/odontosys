d95692046e736635ec114f812b40d3aad6ee2b4b
Gustavo Silva <gustavo_almeida11@hotmail.com>
2023-01-01 13:26:41 -0300

    ms-endereco: handler de MethodArgumentNotValidException no controller advice

--- ms-endereco/src/main/java/br/com/gusta/odontosys/msendereco/data/controllers/EnderecoControllerAdvice.java
+++ ms-endereco/src/main/java/br/com/gusta/odontosys/msendereco/data/controllers/EnderecoControllerAdvice.java
@@ -1,24 +1,33 @@
 package br.com.gusta.odontosys.msendereco.data.controllers;
 
-import br.com.gusta.odontosys.msendereco.core.exceptions.CepInvalidoException;
 import br.com.gusta.odontosys.msendereco.core.exceptions.EnderecoNotFoundException;
 import br.com.gusta.odontosys.msendereco.core.exceptions.MapperException;
+import br.com.gusta.odontosys.msendereco.core.exceptions.ParametroInvalidoException;
+import br.com.gusta.odontosys.msendereco.core.utils.MensagemUtils;
+import br.com.gusta.odontosys.msendereco.data.models.dto.response.error.Campo;
 import br.com.gusta.odontosys.msendereco.data.models.dto.response.error.Problema;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import java.time.LocalDateTime;
-
 @ControllerAdvice
+@RequiredArgsConstructor
 public class EnderecoControllerAdvice extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler(CepInvalidoException.class)
-    public ResponseEntity<Object> handleCepInvalidoException(CepInvalidoException ex, WebRequest request) {
+    private final MessageSource messageSource;
+
+    @ExceptionHandler(ParametroInvalidoException.class)
+    public ResponseEntity<Object> handleCepInvalidoException(ParametroInvalidoException ex, WebRequest request) {
         var status = HttpStatus.BAD_REQUEST;
 
         var erro = new Problema();
@@ -51,5 +60,28 @@ public class EnderecoControllerAdvice extends ResponseEntityExceptionHandler {
         erro.setMensagem(ex.getMessage());
 
         return handleExceptionInternal(ex, erro, new HttpHeaders(), status, request);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers,
+                                                                  HttpStatus status,
+                                                                  WebRequest request) {
+
+        var erroCampos = new ArrayList<Campo>();
+
+        ex.getBindingResult().getAllErrors().forEach(erro -> {
+            var nomeCampo = ((FieldError) erro).getField();
+            var mensagemErro = MensagemUtils.getMensagem(messageSource, erro);
+            erroCampos.add(new Campo(nomeCampo, mensagemErro));
+        });
+
+        var problema = new Problema();
+        problema.setStatus(status.value());
+        problema.setDataHora(LocalDateTime.now());
+        problema.setMensagem("One or more fields are invalid. Fill in correctly and try again.");
+        problema.setCampos(erroCampos);
+
+        return handleExceptionInternal(ex, problema, headers, status, request);
     }
 }
