239b01ed8d5a062457028ff4cb14a4f45873c01f
Gustavo Silva <gustavo_almeida11@hotmail.com>
2023-01-01 13:27:52 -0300

    ms-endereco: correções do controller

--- ms-endereco/src/main/java/br/com/gusta/odontosys/msendereco/data/controllers/EnderecoController.java
+++ ms-endereco/src/main/java/br/com/gusta/odontosys/msendereco/data/controllers/EnderecoController.java
@@ -1,6 +1,6 @@
 package br.com.gusta.odontosys.msendereco.data.controllers;
 
-import br.com.gusta.odontosys.msendereco.core.exceptions.CepInvalidoException;
+import br.com.gusta.odontosys.msendereco.core.exceptions.ParametroInvalidoException;
 import br.com.gusta.odontosys.msendereco.core.utils.MensagemUtils;
 import br.com.gusta.odontosys.msendereco.data.mappers.GenericMapper;
 import br.com.gusta.odontosys.msendereco.data.models.dto.input.NovoEnderecoForm;
@@ -12,6 +12,10 @@ import br.com.gusta.odontosys.msendereco.domain.usecases.BuscarEndereco;
 import br.com.gusta.odontosys.msendereco.domain.usecases.ConsultarEndereco;
 import br.com.gusta.odontosys.msendereco.domain.usecases.DeletarEnderecoExistente;
 import br.com.gusta.odontosys.msendereco.domain.usecases.SalvarNovoEndereco;
+import java.net.URI;
+import java.util.Objects;
+import java.util.stream.Stream;
+import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,11 +30,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.validation.Valid;
-import java.net.URI;
-import java.util.Objects;
-import java.util.stream.Stream;
 
 @Slf4j
 @RestController
@@ -50,31 +49,30 @@ public class EnderecoController {
 
     @GetMapping("/buscar")
     public ResponseEntity<BuscarEnderecoResponse> buscarEndereco(@RequestParam(required = false) String cep) {
-        String mensagem = null;
         if (cep == null) {
-            mensagem = MensagemUtils.getMensagem(messageSource, "cep.nao.passado");
-            log.error(mensagem);
-            throw new CepInvalidoException(mensagem);
+            lancarErro("cep.nao.passado");
         }
 
         var endereco = buscarEndereco.buscarEndereco(cep);
         var response = enderecoBuscarEnderecoResponseMapper.map(endereco);
+
         log.info(MensagemUtils.getMensagem(messageSource, "busca.sucesso"));
+
         return ResponseEntity.ok(response);
     }
 
     @GetMapping
     public ResponseEntity<EnderecoResponse> consultarEndereco(@RequestParam(required = false) String cep,
-                                                              @RequestParam(required = false) int numero) {
-        if (Stream.of(cep, numero).anyMatch(Objects::isNull)) {
-            var mensagem = MensagemUtils.getMensagem(messageSource, "dados.necessarios.nao.informados");
-            log.error(mensagem);
-            throw new CepInvalidoException(mensagem);
+                                                              @RequestParam(required = false) String numero) {
+        if (parametroInvalidos(cep, numero)) {
+            lancarErro("dados.necessarios.nao.informados");
         }
 
-        var endereco = consultarEndereco.consultarEndereco(cep, numero);
+        var endereco = consultarEndereco.consultarEndereco(cep, Integer.parseInt(numero));
         var response = enderecoEnderecoResponseMapper.map(endereco);
+
         log.info(MensagemUtils.getMensagem(messageSource, "consulta.sucesso"));
+
         return ResponseEntity.ok(response);
     }
 
@@ -82,26 +80,32 @@ public class EnderecoController {
     @Transactional
     public ResponseEntity<EnderecoResponse> salvarNovoEndereco(@RequestBody @Valid NovoEnderecoForm form) {
         var novoEndereco = novoEnderecoFormEnderecoMapper.map(form);
+
         var enderecoSalvo = salvarNovoEndereco.salvarNovoEndereco(novoEndereco);
+
         log.info(MensagemUtils.getMensagem(messageSource, "cadastro.sucesso"));
+
         var enderecoResponse = enderecoEnderecoResponseMapper.map(enderecoSalvo);
+
         var uri = URI.create(String.format("/endereco?cep=%s&?numero=%s",
                 enderecoSalvo.getCep(),
                 enderecoSalvo.getNumero()));
+
         return ResponseEntity.created(uri).body(enderecoResponse);
     }
 
     @DeleteMapping
     @Transactional
     public ResponseEntity<Void> deletarEnderecoExistente(@RequestParam(required = false) String cep,
-                                                         @RequestParam(required = false) int numero) {
-        if (Stream.of(cep, numero).anyMatch(Objects::isNull)) {
-            var mensagem = MensagemUtils.getMensagem(messageSource, "dados.necessarios.nao.informados");
-            log.error(mensagem);
-            throw new CepInvalidoException(mensagem);
+                                                         @RequestParam(required = false) String numero) {
+        if (parametroInvalidos(cep, numero)) {
+            lancarErro("dados.necessarios.nao.informados");
         }
-        deletarEnderecoExistente.deletarEndereco(cep, numero);
+
+        deletarEnderecoExistente.deletarEndereco(cep, Integer.parseInt(numero));
+
         log.info(MensagemUtils.getMensagem(messageSource, "remocao.sucesso"));
+
         return ResponseEntity.noContent().build();
     }
 
@@ -110,8 +114,20 @@ public class EnderecoController {
     public ResponseEntity<EnderecoResponse> atualizarEndereco(@RequestBody @Valid NovoEnderecoForm form) {
         var endereco = novoEnderecoFormEnderecoMapper.map(form);
         var enderecoAtualizado = atualizarEndereco.atualizarEndereco(endereco);
+
         log.info(MensagemUtils.getMensagem(messageSource, "remocao.sucesso"));
+
         var response = enderecoEnderecoResponseMapper.map(enderecoAtualizado);
         return ResponseEntity.ok(response);
+    }
+
+    private boolean parametroInvalidos(String cep, String numero) {
+        return Stream.of(cep, numero).anyMatch(Objects::isNull);
+    }
+
+    private void lancarErro(String codigoMensagem) {
+        var mensagem = MensagemUtils.getMensagem(messageSource, codigoMensagem);
+        log.error(mensagem);
+        throw new ParametroInvalidoException(mensagem);
     }
 }
