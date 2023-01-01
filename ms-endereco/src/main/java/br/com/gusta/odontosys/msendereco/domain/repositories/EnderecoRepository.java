34245611623b754124f029ba2a13914816c26aa2
Gustavo Silva <gustavo_almeida11@hotmail.com>
2023-01-01 12:56:05 -0300

    ms-endereco: identação do repository

--- ms-endereco/src/main/java/br/com/gusta/odontosys/msendereco/domain/repositories/EnderecoRepository.java
+++ ms-endereco/src/main/java/br/com/gusta/odontosys/msendereco/domain/repositories/EnderecoRepository.java
@@ -3,8 +3,13 @@ package br.com.gusta.odontosys.msendereco.domain.repositories;
 import br.com.gusta.odontosys.msendereco.domain.entities.Endereco;
 
 public interface EnderecoRepository {
+
     Endereco salvarEndereco(Endereco endereco);
+
     Endereco buscarEndereco(String cep);
+
     Endereco consultarEndereco(String cep, int numero);
+
     void deletarEndereco(String cep, int numero);
+
 }
