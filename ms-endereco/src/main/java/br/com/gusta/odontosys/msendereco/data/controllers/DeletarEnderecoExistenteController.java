6d09e6beab28fd57fe07f318a09302a0f5240744
Gustavo Silva <gustavo_almeida11@hotmail.com>
2023-01-04 13:10:46 -0300

    ms-endereco: carregando dados iniciais para testes

---
+++ ms-endereco/src/main/java/br/com/gusta/odontosys/msendereco/data/controllers/DeletarEnderecoExistenteController.java
@@ -0,0 +1,2 @@
+package br.com.gusta.odontosys.msendereco.data.controllers;public class DeletarEnderecoExistenteController {
+}
---
+++ ms-endereco/src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO endereco (cep, numero, bairro, cidade, complemento, logradouro, uf)
+VALUES('53320-121', 922, 'Bultrins', 'Olinda', 'teste', 'Rua Doutor Epitácio Rodrigues da Silva', 'PE');
