59565c0ec0c5ae1b0e3d5d1eb907f24411dbd079
Gustavo Silva <gustavo_almeida11@hotmail.com>
2023-01-05 19:05:14 -0300

    ms-endereco: utils para testes

--- ms-endereco/src/test/java/br/com/gusta/odontosys/msendereco/utils/JsonUtil.java
+++ ms-endereco/src/test/java/br/com/gusta/odontosys/msendereco/utils/JsonUtil.java
@@ -1,2 +1,18 @@
-package br.com.gusta.odontosys.msendereco.util;public class JsonUtil {
+package br.com.gusta.odontosys.msendereco.utils;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class JsonUtil {
+
+    public String montarJson(Object content) {
+        try {
+            return new ObjectMapper().writeValueAsString(content);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
--- ms-endereco/src/test/java/br/com/gusta/odontosys/msendereco/utils/builder/EnderecoBuilder.java
+++ ms-endereco/src/test/java/br/com/gusta/odontosys/msendereco/utils/builder/EnderecoBuilder.java
@@ -1,2 +1,61 @@
-package br.com.gusta.odontosys.msendereco.utils.builder;public class EnderecoBuilder {
+package br.com.gusta.odontosys.msendereco.utils.builder;
+
+import br.com.gusta.odontosys.msendereco.domain.entities.Endereco;
+
+public class EnderecoBuilder {
+
+    private String cep;
+    private String logradouro;
+    private int numero;
+    private String complemento;
+    private String bairro;
+    private String cidade;
+    private String uf;
+
+    public EnderecoBuilder setCep(String cep) {
+        this.cep = cep;
+        return this;
+    }
+
+    public EnderecoBuilder setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+        return this;
+    }
+
+    public EnderecoBuilder setNumero(int numero) {
+        this.numero = numero;
+        return this;
+    }
+
+    public EnderecoBuilder setComplemento(String complemento) {
+        this.complemento = complemento;
+        return this;
+    }
+
+    public EnderecoBuilder setBairro(String bairro) {
+        this.bairro = bairro;
+        return this;
+    }
+
+    public EnderecoBuilder setCidade(String cidade) {
+        this.cidade = cidade;
+        return this;
+    }
+
+    public EnderecoBuilder setUf(String uf) {
+        this.uf = uf;
+        return this;
+    }
+
+    public Endereco build() {
+        var endereco = new Endereco();
+        endereco.setCep(this.cep);
+        endereco.setLogradouro(this.logradouro);
+        endereco.setNumero(this.numero);
+        endereco.setComplemento(this.complemento);
+        endereco.setBairro(this.bairro);
+        endereco.setCidade(this.cidade);
+        endereco.setUf(this.uf);
+        return endereco;
+    }
 }
