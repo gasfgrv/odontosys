641bf8499256d0b8caa3ef848980459597c7a740
Gustavo Silva <gustavo_almeida11@hotmail.com>
2023-01-05 01:27:04 -0300

    ms-endereco: remoção dos hifens para padronizar o cep salvo na base

--- ms-endereco/src/main/resources/data.sql
+++ ms-endereco/src/main/resources/data.sql
@@ -1,2 +1,2 @@
 INSERT INTO endereco (cep, numero, bairro, cidade, complemento, logradouro, uf)
-VALUES('53320-121', 922, 'Bultrins', 'Olinda', 'teste', 'Rua Doutor Epitácio Rodrigues da Silva', 'PE');
+VALUES('53320121', 922, 'Bultrins', 'Olinda', 'teste', 'Rua Doutor Epitácio Rodrigues da Silva', 'PE');
